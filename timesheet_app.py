894a4c443ca28b4c977bf67d85b5c4a6a9b83fac
Chris Hill <chill7202@gmail.com>
2026-05-10 14:10:02 +1000

    Temp debug: show secret keys

--- timesheet_app.py
+++ timesheet_app.py
@@ -809,6 +809,7 @@ if 'authenticated' not in st.session_state:
 
 if not st.session_state.authenticated:
     st.title("Kingsleyhill Timesheet")
+    st.write("Secret keys found:", list(st.secrets.keys()))
     pwd = st.text_input("Password", type="password")
     if st.button("Sign in"):
         try:
