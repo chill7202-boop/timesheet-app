05b196f7450cc6944bc103ac9d8e8e11a7b31b95
Chris Hill <chill7202@gmail.com>
2026-05-10 16:19:57 +1000

    Update login title to KingsleyHill System

--- timesheet_app.py
+++ timesheet_app.py
@@ -870,7 +870,7 @@ if 'authenticated' not in st.session_state:
     st.session_state.authenticated = False
 
 if not st.session_state.authenticated:
-    st.title("Kingsleyhill")
+    st.title("KingsleyHill System")
     pwd = st.text_input("Password", type="password")
     if st.button("Sign in"):
         try:
